bc7dd7b833421928bca1a18e3da2a571fc626951
angeljiang0927@gmail.com <angeljiang0927@gmail.com>
2020-11-28 11:51:06 -0800

    added stack view for question views

---
+++ Quiz/DetailViewController.swift
@@ -0,0 +1,15 @@
+//
+//  DetailViewController.swift
+//  Quiz
+//
+//  Created by Angel Jiang on 11/28/20.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    @IBOutlet var questionField: UITextField!
+    @IBOutlet var answerField: UITextField!
+    @IBOutlet var dateLabel: UILabel!
+}
